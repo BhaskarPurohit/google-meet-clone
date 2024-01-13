883256cd643ee8cfba26e6ff4d5fac42355a7d4a
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-01-13 22:27:59 +0530

    setup usePeer

---
+++ hooks/usePeer.js
@@ -0,0 +1,8 @@
+const { useState, useEffect } = require("react")
+
+const usePeer = ()=>{
+  const [peer, setPeer] = useState(null)
+  const [myId, setMyId] = useState()  //used to store id that we get from the server
+
+  useEffect(()=>{},[])
+}
